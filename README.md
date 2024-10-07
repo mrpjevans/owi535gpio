acd51815aa000ac5881808b7a8830dfcabbad0b8
PJ Evans <pj@mrpjevans.com>
2024-10-07 15:16:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # owi535gpio
-Sctipts for controlling a OWI-535 robot arm using Raspberry Pi's GPIO
+Scripts for controlling a OWI-535 robot arm using Raspberry Pi's GPIO. This is a proof-of-concept
+for GPIO-based control of a robot arm using H-bridges.
+
+## You Will Need
+
+- Raspberry Pi
+- 3 x L298N H-Bridge boards
+- OWI 535 Edge Robot Arm
+- Jumper cables
+
+## Wire-up
+
+The L298N boards will need their own 12V power supply
+
+Ensure there is a ground cable between the boards and the Raspberry Pi
+
+For each board, wire up to the GPIO as follows:
+
+|L298N 1|GPIO|
+|-|-|
+|Shoulder|
+|EN1|16|
+|IN1|20|
+|IN2|21|
+|Base|
+|IN3|19|
+|IN4|13|
+|EN2|26|
+
+|L298N 2|GPIO|
+|-|-|
+|Wrist|
+|EN1|14|
+|IN1|15|
+|IN2|18|
+|Elbow|
+|IN3|3|
+|IN4|4|
+|EN2|2|
+
+|L298N 3|GPIO|
+|-|-|
+|Grip|
+|EN1|22|
+|IN1|27|
+|IN2|17|
+
+Now connect each motor connector on the L298Ns to the relevant motos on the robot arm.
+
+## Software
+
+Activate the local environment
+
+```
+ca /path/to/repo
+source ./bin/activate
+```
+
+Run `test.py`
+
+```
+python3 test.py
+```
+
+## Control
+
+1-5: Select motor
+A: Backward
+D: Forward
+F: Stop
+W: Increase speed
+S: Decrease speed
+
+Exit: ctrl+c
--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 from time import sleep
 
-import keyboard
 from gpiozero import Motor
 
 grip = Motor(27, 17, enable=22)
+grip.stop()
 wrist = Motor(15, 18, enable=14)
+wrist.stop()
 elbow = Motor(3, 4, enable=2)
+elbow.stop()
 shoulder = Motor(20, 21, enable=16)
+shoulder.stop()
 base = Motor(19, 13, enable=26)
+base.stop()
 
-def tick():
-    pass
-
+selected = base
+speed = 6
 
 def getchar():
    #Returns a single character from standard input
@@ -29,11 +32,41 @@ while True:
     ch = getchar()
 
     match(ch):
+        case "1":
+            selected.stop()
+            selected = base
+            print("Base motor selected")
+        case "2":
+            selected.stop()
+            selected = grip
+            print("Grip motor selected")
+        case "3":
+            selected.stop()
+            selected = wrist
+            print("Wrist motor selected")
+        case "4":
+            selected.stop()
+            selected = elbow
+            print("Elbow motor selected")
+        case "5":
+            selected.stop()
+            selected = shoulder
+            print("Shoulder motor selected")
+        case "w":
+            if speed < 10:
+                speed += 2
+            print("Speed is ", speed)
+        case "s":
+            if speed > 0:
+                speed -= 2
+            print("Speed is ", speed)
         case "a":
-            print("A")
-
-    if ch.strip() == '':
-        print('bye!')
-        break
-
-    sleep(0.1)
+            selected.backward(speed / 10)
+            print("Backwards")
+        case "d":
+            selected.forward(speed / 10)
+            print("Forwards")
+        case "f":
+            selected.stop()
+            print("Stop")
+            
