faf75970b192fe1a55ac78f6f632efaea616dd2a
PJ Evans <pj@mrpjevans.com>
2024-10-08 17:09:04 +0100

    Tidy up

---
+++ pico/MQTT_CONFIG.example.py
@@ -0,0 +1,4 @@
+# Complete the details to match your MQTT broker and save as MQTT_CONFIG.py
+MQTT_CLIENT_ID = b'owi535'
+MQTT_BROKER = "127.0.0.1"
+MQTT_SUB_TOPIC = b'topic'
---
+++ pico/WIFI_CONFIG.example.py
@@ -0,0 +1,4 @@
+# Complete the details to match your network and save as WIFI_CONFIG.py
+SSID = "YourWifiNetwork"
+PSK = "WifiPassword"
+COUNTRY = "gb"
