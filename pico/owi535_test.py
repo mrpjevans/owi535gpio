4c8583d73acfc6c1f9e9b65e6a76241757fb14ba
PJ Evans <pj@mrpjevans.com>
2025-06-05 10:46:00 +0100

    fix: simplify pins assignments

--- pico/owi535_test.py
+++ pico/owi535_test.py
@@ -3,24 +3,8 @@ from time import sleep
 
 from dcmotor import DCMotor
 
-#
-# Uncomment the motor_pins for the motor you want to test
-#
-
-# Base
-motor_pins = [4,3,2]
-
-# Shoulder
-#motor_pins = [15,18,14]
-
-# Grip
-#motor_pins = [27,22,17]
-
-# Wrist
-#motor_pins = [26,19,13]
-
-# Elbow
-#motor_pins = [16,20,21]
+# Pins (Motor 1 , Motor 2, Enable)
+motor_pins = [2,3,1]
 
 frequency = 1000
 speed = 10
